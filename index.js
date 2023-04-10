0d77682790add24fe9ff6efe64315e4f1a3e8c62
brendanjeffreymcmahon <brendan.mcmahon@bloomerang.com>
2023-04-10 16:01:47 -0400

    CORS

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ exports.handler = async (event) => {
 
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify(data),
     };
   } catch (error) {
