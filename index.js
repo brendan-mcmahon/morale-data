f234f32d4ee5c4262c26214f5a035cc6f2ec8747
brendanjeffreymcmahon <brendan.mcmahon@bloomerang.com>
2023-04-10 12:58:35 -0400

    all of it

---
+++ index.js
@@ -0,0 +1,29 @@
+const AWS = require("aws-sdk");
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+  try {
+    const data = await fetchData();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    console.error("Error fetching data", error);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error),
+    };
+  }
+};
+
+const fetchData = async () => {
+  const params = {
+    TableName: "morale",
+  };
+
+  const result = await dynamodb.scan(params).promise();
+  return result.Items;
+};
